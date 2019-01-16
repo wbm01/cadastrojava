bc7b1c1765ca949fb82ef457711793d2372761a5
wbm01 <46757147+wbm01@users.noreply.github.com>
2019-01-16 15:17:21 -0200

    Add files via upload

---
+++ CadastroJava.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadastrojava;
+
+/**
+ *
+ * @author Willian
+ */
+public class CadastroJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
